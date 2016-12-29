155c8a0301db3994d5489eb9d14441516f9abb17
boeckMt <bbmatdd@gmail.com>
2016-12-29 13:09:04 +0100

    add/remove layer from overlays with button

--- examples/addlayer.js
+++ examples/addlayer.js
@@ -1,49 +1,75 @@
 (function() {
 
-    // Create a group for overlays. Add the group to the map when it's created
-    // but add the overlay layers later
-    var overlayGroup = new ol.layer.Group({
-        title: 'Overlays',
+  // Create a group for overlays. Add the group to the map when it's created
+  // but add the overlay layers later
+  var overlayGroup = new ol.layer.Group({
+    title: 'Overlays',
+    layers: []
+  });
+
+
+  var testLayer = new ol.layer.Tile({
+    title: 'Countries',
+    source: new ol.source.TileWMS({
+      url: 'http://demo.opengeo.org/geoserver/wms',
+      params: {
+        'LAYERS': 'ne:ne_10m_admin_1_states_provinces_lines_shp'
+      },
+      serverType: 'geoserver'
+    })
+  });
+
+
+  var addbutton = document.getElementById('addLayer');
+  addbutton.addEventListener("click", addLayer, false);
+
+  var removebutton = document.getElementById('removeLayer');
+  removebutton.addEventListener("click", removeLayer, false);
+
+  // Create a map containing two group layers
+  var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Group({
+        'title': 'Base maps',
         layers: [
+          new ol.layer.Tile({
+            title: 'OSM',
+            type: 'base',
+            source: new ol.source.OSM()
+          })
         ]
-    });
+      }),
+      overlayGroup
+    ],
+    view: new ol.View({
+      center: ol.proj.transform([-0.92, 52.96], 'EPSG:4326', 'EPSG:3857'),
+      zoom: 6
+    })
+  });
 
-    // Create a map containing two group layers
-    var map = new ol.Map({
-        target: 'map',
-        layers: [
-            new ol.layer.Group({
-                'title': 'Base maps',
-                layers: [
-                    new ol.layer.Tile({
-                        title: 'OSM',
-                        type: 'base',
-                        source: new ol.source.OSM()
-                    })
-                ]
-            }),
-            overlayGroup
-        ],
-        view: new ol.View({
-            center: ol.proj.transform([-0.92, 52.96], 'EPSG:4326', 'EPSG:3857'),
-            zoom: 6
-        })
-    });
+  // Create a LayerSwitcher instance and add it to the map
+  var layerSwitcher = new ol.control.LayerSwitcher();
+  map.addControl(layerSwitcher);
 
-    // Create a LayerSwitcher instance and add it to the map
-    var layerSwitcher = new ol.control.LayerSwitcher();
-    map.addControl(layerSwitcher);
+  // Add a layer to a pre-exiting ol.layer.Group after the LayerSwitcher has
+  // been added to the map. The layer will appear in the list the next time
+  // the LayerSwitcher is shown or LayerSwitcher#renderPanel is called.
+  function addLayer() {
+    overlayGroup.getLayers().push(testLayer);
+  }
 
-    // Add a layer to a pre-exiting ol.layer.Group after the LayerSwitcher has
-    // been added to the map. The layer will appear in the list the next time
-    // the LayerSwitcher is shown or LayerSwitcher#renderPanel is called.
-    overlayGroup.getLayers().push(new ol.layer.Tile({
-        title: 'Countries',
-        source: new ol.source.TileWMS({
-            url: 'http://demo.opengeo.org/geoserver/wms',
-            params: {'LAYERS': 'ne:ne_10m_admin_1_states_provinces_lines_shp'},
-            serverType: 'geoserver'
-        })
-    }));
+  function removeLayer() {
+    var collection = overlayGroup.getLayers();
+
+    collection.forEach(function(lay, index){
+      //console.log(lay, index);
+      if(lay === testLayer){
+        console.log(lay, index);
+        this.removeAt(index);
+      }
+    },collection);
+  }
+
 
 })();
